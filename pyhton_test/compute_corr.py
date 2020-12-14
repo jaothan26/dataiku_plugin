ae6cbb1ebbd188288b59d7520578c42fcbeb1139
admin <jaothan26@gmail.com>
2020-12-14 14:13:04 +0100

    Edited files

---
+++ pyhton_test/compute_corr.py
@@ -0,0 +1,8 @@
+ import pandas as pd , numpy as np 
+    
+    def __init__(self, cluster_id, cluster_name, config, plugin_config):
+        self.cluster_id = cluster_id
+        self.cluster_name = cluster_name
+        self.config = config
+        self.plugin_config = plugin_config
+        
